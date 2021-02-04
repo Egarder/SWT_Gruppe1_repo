e3b07c8c16a2ca59817b684eee2ebbe7fa248cc9
Egarder <61559853+Egarder@users.noreply.github.com>
2021-02-04 13:52:53 +0100

    added overloads again

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -12,11 +12,23 @@ namespace Calculate
             Accumulator = temp;
             return (a+b);
         }
+
+        public double Add(double a)
+        {
+            return a + Accumulator;
+        }
+
+
         public double Substract(double a, double b)
         {
             double temp = (a-b);
             Accumulator = temp;
             return (a-b);
+        }
+
+        public double Substract(double a)
+        {
+            return a - Accumulator;
         }
         public double Multiply(double a, double b)
         {
